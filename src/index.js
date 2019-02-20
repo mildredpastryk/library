6cdaf9c5c6482d90c1cf062aa4686764d44cd2b5
Mildred Pastryk <mildred.pastryk@gmail.com>
2019-02-20 12:55:23 +0100

    add dynamic data from array

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react'
 import { render } from 'react-dom'
 import './App.css';
 
+let bookList = [
+	{"title": "Business at the Speed of Thought", "author": "Bill Gates", "pages": 496},
+	{"title": "Buffett's Bites", "author": "Warren Buffet", "pages": 224},
+	{"title": "My Life & Work", "author": "Henry Ford", "pages": 188},
+	{"title": "The 7 Habits of Highly Effective People", "author": "Stephen R. Covey", "pages": 432},
+    {"title": "High Performance Habits", "author": "Brendon Burchard", "pages": 400}
+]
 
 const Book = ({title, author, pages}) => {
 	return (
@@ -13,18 +20,22 @@ const Book = ({title, author, pages}) => {
 	)
 }
 
-const Library = () => {
+const Library = ({books}) => {
 	return (
 		<div>
             <h1 className="App-title">My Library</h1>
-			<Book title="The Sun Also Rises" author="Ernest Hemingway" pages={260}/>
-			<Book title="White Teeth" author="Zadie Smith" pages={480}/>
-			<Book title="Cat's Cradle" author="Kurt Vonnegut" pages={304}/>
+            {books.map (
+                book => 
+                    <Book 
+                        title={book.title} 
+                        author={book.author} 
+                        pages={book.pages}/>
+            )}
 		</div>
 	)
 }
 
 render(
-    <Library />, 
+    <Library books={bookList} />, 
 	document.getElementById('root')
 )
