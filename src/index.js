afd73f359fd5ca45b1f1e3f727763ca770af1d3d
Mildred Pastryk <mildred.pastryk@gmail.com>
2019-02-21 09:42:10 +0100

    add default props - use default arguments syntax in a function component

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import { render } from 'react-dom'
 import './App.css';
 
 let bookList = [
-	{"title": "Business at the Speed of Thought", "author": "Bill Gates", "pages": 496},
+	{"title": "Business at the Speed of Thought", "pages": 496},
 	{"title": "Buffett's Bites", "author": "Warren Buffet", "pages": 224},
 	{"title": "My Life & Work", "author": "Henry Ford", "pages": 188},
 	{"title": "The 7 Habits of Highly Effective People", "author": "Stephen R. Covey", "pages": 432},
     {"title": "High Performance Habits", "author": "Brendon Burchard", "pages": 400}
 ]
 
-const Book = ({title, author, pages, freeBookmark}) => {
+const Book = ({title="No Title Provided", author="No Author", pages=0, freeBookmark}) => {
 	return (
 		<section>
 			<h2>{title}</h2>
@@ -105,6 +105,6 @@ class Library extends React.Component {
 }
 
 render(
-    <Library  />, 
+    <Library books={bookList} />, 
 	document.getElementById('root')
 )
