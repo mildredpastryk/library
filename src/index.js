7f07543e7326452002c7fd947196d54be1f523bc
Mildred Pastryk <mildred.pastryk@gmail.com>
2019-02-21 11:07:22 +0100

    add prop types to provide right types to components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
 import './App.css';
+import PropTypes from 'prop-types'
 
 let bookList = [
-	{"title": "Business at the Speed of Thought", "pages": 496},
+	{"title": "Business at the Speed of Thought", "author": "Bill Gates", "pages": 496},
 	{"title": "Buffett's Bites", "author": "Warren Buffet", "pages": 224},
 	{"title": "My Life & Work", "author": "Henry Ford", "pages": 188},
 	{"title": "The 7 Habits of Highly Effective People", "author": "Stephen R. Covey", "pages": 432},
@@ -102,6 +103,17 @@ class Library extends React.Component {
 		 </div>
 	    )
     }
+}
+
+Library.propTypes = {
+	books: PropTypes.array
+}
+
+Book.propTypes = {
+	title: PropTypes.string,
+	author: PropTypes.string,
+	pages: PropTypes.number,
+	freeBookmark: PropTypes.bool
 }
 
 render(
